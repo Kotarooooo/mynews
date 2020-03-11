296bb8008c06340408f1de39598b16ed27e4cfcf
石田浩太郎 <wtrlndhr@gmail.com>
2020-03-11 12:02:20 +0000

    lalavel13

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,9 @@ Route::get('/', function () {
 });
 Route::group(['prefix' => 'admin'], function() {
     Route::get('news/create', 'Admin\NewsController@add');
+Route::post('news/create', 'Admin\NewsController@create');
+Route::post('profile/create', 'Admin\ProfileController@create');
+Route::post('profile/edit', 'Admin\ProfileController@update');
 });
 Route::get('admin/news/create', 'Admin\NewsController@add');
 Route::get('XXXXXX', 'Admin\AAAController@bbb');
@@ -24,3 +27,16 @@ Route::get('admin/profile/edit', 'Admin\ProfileController@edit');
 
 
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
+});    
+});
+});
