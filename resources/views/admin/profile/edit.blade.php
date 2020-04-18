47c781b65dedf64ef0fa4a1a308d912262c30cb3
石田浩太郎 <wtrlndhr@gmail.com>
2020-04-18 02:30:23 +0000

    laravel16訂正2

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.admin')
+@extends('layouts.profile')
 @section('title', 'My profile')
 
 @section('content')
@@ -19,25 +19,25 @@
                     <div class="form-group row">
                         <label class="col-md-2">氏名</label>
                         <div class="col-md-10">
-                            <input type="text" class="form-control" name="name" value="{{ old('name') }}">
+                            <input type="text" class="form-control" name="name" value="{{ $profile_form->name }}">
                         </div>
                     </div>
                     <div class="form-group row">
                         <label class="col-md-2">性別</label>
                         <div class="col-md-10">
-                            <input type="text" class="form-control" name="gender" value="{{ old('gender') }}">
+                            <input type="text" class="form-control" name="gender" value="{{$profile_form->gender  }}">
                         </div>
                     </div>    
                     <div class="form-group row">
                         <label class="col-md-2">自己紹介欄</label>
                         <div class="col-md-10">
-                            <input type="text" class="form-control" name="introduction" value="{{ old('introduction') }}">
+                            <input type="text" class="form-control" name="introduction" value="{{ $profile_form->introduction }}">
                         </div>    
                     </div>
                     <div class="form-group row">
                         <label class="col-md-2">趣味</label>
                         <div class="col-md-10">
-                            <input type="text" class="form-control" name="hobby" value="{{ old('hobby') }}">
+                            <input type="text" class="form-control" name="hobby" value="{{ $profile_form->hobby }}">
                         </div>
                     </div>
                     <div class="form-group row">
@@ -48,18 +48,7 @@
                         </div>
                     </div>
                 </form>
-            <div class="row mt-5">
-                    <div class="col-md-4 mx-auto">
-                        <h2>編集履歴</h2>
-                        <ul class="list-group">
-                            @if ($profile_form->histories != NULL)
-                                @foreach ($profile_form->histories as $history)
-                                    <li class="list-group-item">{{ $history->edited_at }}</li>
-                                @endforeach
-                            @endif
-                        </ul>
-                    </div>
-                </div>
+                
             </div>
         </div>
     </div>
